0117705936fa4c76b0ea395e05f844c3c00ea95f
Tom Lau <hello.iamtlau@gmail.com>
2018-09-30 03:44:41 -0700

    fixed folder structure, got rid of extra app-swoosh folder

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Tom Lau on 9/25/18.
+//  Copyright © 2018 TL Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
